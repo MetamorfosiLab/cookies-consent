61715422ced615531b2ab3dfea1b43e15b111371
hrynevych.romann <hrynevych.romann@gmail.com>
2024-01-29 22:33:37 +0200

    fix: add styles and themes to unbuild config

--- build.config.ts
+++ build.config.ts
@@ -3,6 +3,16 @@ import { defineBuildConfig } from 'unbuild'
 export default defineBuildConfig({
   entries: [
     'src/index',
+    {
+      builder: 'mkdist',
+      input: './src/styles/',
+      outDir: './dist/styles/',
+    },
+    {
+      builder: 'mkdist',
+      input: './src/themes/',
+      outDir: './dist/themes/',
+    },
   ],
   declaration: true,
   clean: true,
